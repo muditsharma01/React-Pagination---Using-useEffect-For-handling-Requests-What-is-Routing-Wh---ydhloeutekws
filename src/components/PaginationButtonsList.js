9315be554c0c9523e3a9c56b956b53ee98d2de53
muditsharma01 <muditshar001@gmail.com>
2023-01-29 19:22:54 +0530

    Update PaginationButtonsList.js

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,11 +1,20 @@
-import React from 'react'
-const PaginationButtonsList = () => {
+import React from 'react';
 
-    return (
-        <div className="pagination-buttons-list">
+const PaginationButtonList = ({ currentPage, setCurrentPage }) => {
+  return (
+    <div className="pagination-button-list">
+      {Array.from({ length: 20 }, (_, i) => (
+        <button
+          key={i + 1}
+          id={`button-${i + 1}`}
+          className={`page-btn ${currentPage === i + 1 ? 'active-btn' : ''}`}
+          onClick={() => setCurrentPage(i + 1)}
+        >
+          {i + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export { PaginationButtonsList }
+export default PaginationButtonList;
